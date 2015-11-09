50ecc8e6969eaf67eca19d0f3bf04c6d5c132a5f
Alex Rothuis <arothuis@ibuildings.nl>
2015-11-09 13:28:05 +0100

    Removes attribute injection from controller

--- src/OpenConext/ProfileBundle/Controller/ProfileController.php
+++ src/OpenConext/ProfileBundle/Controller/ProfileController.php
@@ -24,8 +24,6 @@ class ProfileController extends Controller
 {
     public function overviewAction()
     {
-        $attributes = $this->getUser()->getAttributes();
-
-        return $this->render('OpenConextProfileBundle:Profile:overview.html.twig', ['attributes' => $attributes]);
+        return $this->render('OpenConextProfileBundle:Profile:overview.html.twig');
     }
 }
