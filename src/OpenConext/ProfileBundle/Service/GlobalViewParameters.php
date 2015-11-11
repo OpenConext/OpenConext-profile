33152d151ff78d19d33438a21be3c7c2da15a5d2
Alex Rothuis <arothuis@ibuildings.nl>
2015-11-11 12:16:15 +0100

    Reworks and simplifies GlobalViewParameters
    GlobalViewParameters' factory method has been removed because
    it was not necessary and could lead to wrong expectations. Parameter
    validation is now done in a simplified and more testable manner.
    Also, the correct interface is now depended upon in the constructor.

--- src/OpenConext/ProfileBundle/Service/GlobalViewParameters.php
+++ src/OpenConext/ProfileBundle/Service/GlobalViewParameters.php
@@ -21,11 +21,12 @@ namespace OpenConext\ProfileBundle\Service;
 use InvalidArgumentException;
 use OpenConext\Profile\Assert;
 use Symfony\Component\Translation\DataCollectorTranslator;
+use Symfony\Component\Translation\TranslatorInterface;
 
-class GlobalViewParameters
+final class GlobalViewParameters
 {
     /**
-     * @var DataCollectorTranslator
+     * @var TranslatorInterface
      */
     private $translator;
 
@@ -38,23 +39,29 @@ class GlobalViewParameters
      * @var array
      */
     private $platformUrls;
+
     /**
      * @var array
      */
     private $termsOfServiceUrls;
 
     /**
-     * @param DataCollectorTranslator $translator
+     * @param TranslatorInterface $translator
+     * @param array $locales
      * @param array $helpUrls
      * @param array $platformUrls
      * @param array $termsOfServiceUrls
      */
-    private function __construct(
-        DataCollectorTranslator $translator,
+    public function __construct(
+        TranslatorInterface $translator,
+        array $locales,
         array $helpUrls,
         array $platformUrls,
         array $termsOfServiceUrls
     ) {
+        Assert::keysAre($helpUrls, $locales);
+        Assert::keysAre($platformUrls, $locales);
+        Assert::keysAre($termsOfServiceUrls, $locales);
 
         $this->translator = $translator;
         $this->helpUrls = $helpUrls;
@@ -63,68 +70,11 @@ class GlobalViewParameters
     }
 
     /**
-     * @param DataCollectorTranslator $translator
-     * @param array $locales
-     * @param array $helpUrls
-     * @param array $platformUrls
-     * @param array $termsOfServiceUrls
-     * @return GlobalViewParameters
-     */
-    public static function createForLocales(
-        DataCollectorTranslator $translator,
-        array $locales,
-        array $helpUrls,
-        array $platformUrls,
-        array $termsOfServiceUrls
-    ) {
-        if (!in_array($translator->getLocale(), $locales)) {
-            throw new InvalidArgumentException(sprintf(
-                'Locale "%s" is not configured as a valid locale. Currently configured locales: %s',
-                $translator->getLocale(),
-                implode(', ', $locales)
-            ));
-        }
-
-        foreach ($locales as $locale) {
-            Assert::keyExists($helpUrls, $locale, sprintf('Locale "%s" not configured for helpUrls', $locale));
-            Assert::keyExists($platformUrls, $locale, sprintf('Locale "%s" not configured for platformUrls', $locale));
-            Assert::keyExists($termsOfServiceUrls, $locale, sprintf(
-                'Locale "%s" not configured for termsOfServiceUrls',
-                $locale
-            ));
-        }
-
-        self::checkIfNoExtraLocales($helpUrls, $locales, 'helpUrls');
-        self::checkIfNoExtraLocales($platformUrls, $locales, 'platformUrls');
-        self::checkIfNoExtraLocales($termsOfServiceUrls, $locales, 'termsOfServiceUrls');
-
-        return new self($translator, $helpUrls, $platformUrls, $termsOfServiceUrls);
-    }
-
-    /**
-     * @param $translations
-     * @param $allowedLocales
-     * @param $name
-     */
-    private static function checkIfNoExtraLocales($translations, $allowedLocales, $name)
-    {
-        $localeDifference = array_diff(array_keys($translations), $allowedLocales);
-
-        if (!empty($localeDifference)) {
-            throw new InvalidArgumentException(sprintf(
-                'Unconfigured locales defined for %s: %s',
-                $name,
-                implode(', ', $localeDifference)
-            ));
-        }
-    }
-
-    /**
      * @return array
      */
     public function getHelpUrl()
     {
-        return $this->resolveLocale($this->helpUrls, $this->translator->getLocale());
+        return $this->helpUrls[$this->translator->getLocale()];
     }
 
     /**
@@ -132,7 +82,7 @@ class GlobalViewParameters
      */
     public function getPlatformUrl()
     {
-        return $this->resolveLocale($this->platformUrls, $this->translator->getLocale());
+        return $this->platformUrls[$this->translator->getLocale()];
     }
 
     /**
@@ -140,20 +90,6 @@ class GlobalViewParameters
      */
     public function getTermsOfServiceUrl()
     {
-        return $this->resolveLocale($this->termsOfServiceUrls, $this->translator->getLocale());
-    }
-
-    /**
-     * @param array $configuration
-     * @param $locale
-     * @return string
-     */
-    private function resolveLocale(array $configuration, $locale)
-    {
-        if ($configuration[$locale]) {
-            return $configuration[$locale];
-        }
-
-        return '';
+        return $this->termsOfServiceUrls[$this->translator->getLocale()];
     }
 }
