695e88b321ec62b1ca1724e994ea0f8a340d6af8
Alex Rothuis <arothuis@ibuildings.nl>
2015-11-18 10:36:20 +0100

    Removes commented out code in DependencyInjection

--- src/OpenConext/ProfileBundle/DependencyInjection/Configuration.php
+++ src/OpenConext/ProfileBundle/DependencyInjection/Configuration.php
@@ -26,7 +26,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-//        $rootNode = $treeBuilder->root('open_conext_profile');
         $treeBuilder->root('open_conext_profile');
 
         return $treeBuilder;
--- src/OpenConext/ProfileBundle/DependencyInjection/OpenConextProfileExtension.php
+++ src/OpenConext/ProfileBundle/DependencyInjection/OpenConextProfileExtension.php
@@ -27,9 +27,6 @@ class OpenConextProfileExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-//        $configuration = new Configuration();
-//        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('controllers.yml');
         $loader->load('services.yml');
