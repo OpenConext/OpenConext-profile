ff98393f2f2a3b7ac5de181281b1d5f5c828ccaf
Alex Rothuis <arothuis@ibuildings.nl>
2015-11-20 13:51:40 +0100

    Fixes ServicesController as a service

--- src/OpenConext/ProfileBundle/Controller/ServicesController.php
+++ src/OpenConext/ProfileBundle/Controller/ServicesController.php
@@ -18,28 +18,66 @@
 
 namespace OpenConext\ProfileBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use OpenConext\ProfileBundle\Service\ConsentService;
+use Surfnet\SamlBundle\SAML2\Attribute\AttributeDictionary;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Templating\EngineInterface;
 
-class ServicesController extends Controller
+class ServicesController
 {
+    /**
+     * @var EngineInterface
+     */
+    private $templateEngine;
+
+    /**
+     * @var AttributeDictionary
+     */
+    private $attributeDictionary;
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+    /**
+     * @var ConsentService
+     */
+    private $consentService;
+
+    /**
+     * @param EngineInterface $templateEngine
+     * @param AttributeDictionary $attributeDictionary
+     * @param TokenStorageInterface $tokenStorage
+     * @param ConsentService $consentService
+     */
+    public function __construct(
+        EngineInterface $templateEngine,
+        AttributeDictionary $attributeDictionary,
+        TokenStorageInterface $tokenStorage,
+        ConsentService $consentService
+    ) {
+        $this->templateEngine = $templateEngine;
+        $this->attributeDictionary = $attributeDictionary;
+        $this->tokenStorage = $tokenStorage;
+        $this->consentService = $consentService;
+    }
+
     public function overviewAction()
     {
-        /** @var \Surfnet\SamlBundle\SAML2\Attribute\AttributeDictionary $attributeDictionary */
-        $attributeDictionary = $this->get('surfnet_saml.saml.attribute_dictionary');
+        $userIdDefinition = $this->attributeDictionary
+            ->getAttributeDefinitionByUrn('urn:oid:1.3.6.1.4.1.1076.20.40.40.1');
 
-        // The urn entered here should refer to an attribute containing a user id
-        $userIdDefinition = $attributeDictionary->getAttributeDefinitionByUrn('urn:oid:1.3.6.1.4.1.1076.20.40.40.1');
-
-        $user = $this->getUser();
+        $user = $this->tokenStorage->getToken()->getUser();
 
         /** @var \Surfnet\SamlBundle\SAML2\Attribute\AttributeSet $attributeSet */
         $attributeSet = $user->getAttributes();
         $userId = $attributeSet->getAttributeByDefinition($userIdDefinition);
 
-        /** @var \OpenConext\ProfileBundle\Service\ConsentService $consentService */
-        $consentService = $this->get('profile.service.consent');
-        $consents = $consentService->findAllFor($userId->getValue());
+        $consents = $this->consentService->findAllFor($userId->getValue());
 
-        return $this->render('OpenConextProfileBundle:Services:overview.html.twig', ['consents' => $consents]);
+        return new Response($this->templateEngine->render(
+            'OpenConextProfileBundle:Services:overview.html.twig',
+            ['consents' => $consents]
+        ));
     }
 }
