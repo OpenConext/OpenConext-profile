76ada4a5849494cb8a32866748f683ec6ee9c319
Alex Rothuis <arothuis@ibuildings.nl>
2015-11-24 13:18:42 +0100

    Changes JavaScript to only use one var statement at top

--- web/js/my-services.js
+++ web/js/my-services.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('html').removeClass('no-js');
 
     $(document).on('click', '[data-service-details-state] .service-details-state-toggle', function (e) {
-        var serviceDetailsState = $(e.target).parents('[data-service-details-state]');
-        var serviceDetailsToggle = serviceDetailsState.attr('data-service-details-state');
+        var serviceDetailsState = $(e.target).parents('[data-service-details-state]'),
+            serviceDetailsToggle = serviceDetailsState.attr('data-service-details-state');
 
         if (serviceDetailsToggle === 'closed' || serviceDetailsToggle === 'initial') {
             serviceDetailsState.attr('data-service-details-state', 'open');
