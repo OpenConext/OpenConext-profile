39a429e0a0fa4f7325ac971de4ad868f28189dff
Alex Rothuis <arothuis@ibuildings.nl>
2015-11-24 11:34:08 +0100

    Decouples JavaScript from HTML structure
    Introduces service-details-state data-attribute which reflects whether service-details
    for the relevant service are to be shown or hidden. This state can be altered via
    the clickable service-details-state-toggle.

--- web/js/my-services.js
+++ web/js/my-services.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+    $('html').removeClass('no-js');
 
-    // To support non-js visitors, hide service details if JavaScript is enabled
-    $('.service-name .caret').addClass('fa-angle-right').removeClass('fa-angle-down');
-    $('.service-details').addClass('hidden');
+    $(document).on('click', '[data-service-details-state] .service-details-state-toggle', function (e) {
+        var serviceDetailsState = $(e.target).parents('[data-service-details-state]');
+        var serviceDetailsToggle = serviceDetailsState.attr('data-service-details-state');
 
-    $('.service-name').click(function() {
-        $('.service-name .caret', $(this).parent()).toggleClass('fa-angle-right').toggleClass('fa-angle-down');
-        $('.service-details', $(this).parent()).toggleClass('hidden');
+        if (serviceDetailsToggle === 'closed' || serviceDetailsToggle === 'initial') {
+            serviceDetailsState.attr('data-service-details-state', 'open');
+        } else {
+            serviceDetailsState.attr('data-service-details-state', 'closed');
+        }
     });
 });
