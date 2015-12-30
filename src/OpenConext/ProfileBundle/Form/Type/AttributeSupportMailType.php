c0da74f1fa10e7bffcba0365f8fe54a28ddf4699
Alex Rothuis <arothuis@ibuildings.nl>
2015-12-30 14:57:55 +0100

    Adds attribute support mail form

---
+++ src/OpenConext/ProfileBundle/Form/Type/AttributeSupportMailType.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Copyright 2015 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\ProfileBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class AttributeSupportMailType extends AbstractType
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->setAction($this->urlGenerator->generate('profile.attribute_support_send_mail'))
+            ->add(
+                'sendMail',
+                'submit',
+                [
+                    'label' => 'profile.attribute_support.send_mail',
+                    'attr'  =>
+                        [
+                            'class' =>
+                                'mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect'
+                        ]
+                ]
+            );
+    }
+
+    public function getName()
+    {
+        return 'profile_attribute_support_mail';
+    }
+}
