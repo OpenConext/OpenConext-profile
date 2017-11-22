87a28d2f1d2d50cb9b36362e42d676d7dd16a904
Michiel Kodde <mkodde@ibuildings.nl>
2017-11-22 14:01:11 +0100

    Create and install the AA API client bundle
    The bundle is a rough copy of the EngineBlock API client.

---
+++ src/OpenConext/AttributeAggregationApiClientBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Copyright 2017 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\AttributeAggregationApiClientBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('open_conext_attribute_aggregation');
+
+        $rootNode
+            ->children()
+                ->arrayNode('http_client')
+                    ->isRequired()
+                    ->children()
+                        ->scalarNode('base_url')
+                            ->info('The base URL of the Attribute Aggregation API host')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                            ->validate()
+                                ->ifTrue(function ($baseUrl) {
+                                    return !is_string($baseUrl);
+                                })
+                                ->thenInvalid('The Attribute Aggregation API base URL should be a string')
+                            ->end()
+                            ->validate()
+                                ->ifTrue(function ($baseUrl) {
+                                    return !filter_var($baseUrl, FILTER_VALIDATE_URL);
+                                })
+                                ->thenInvalid('The Attribute Aggregation API base URL should be a valid URL')
+                            ->end()
+                            ->validate()
+                                ->ifTrue(function ($baseUrl) {
+                                    $path = parse_url($baseUrl, PHP_URL_PATH);
+
+                                    return $path[strlen($path)-1] !== '/';
+                                })
+                                ->thenInvalid('The Attribute Aggregation API base URL must end in a forward slash')
+                            ->end()
+                        ->end()
+                        ->booleanNode('verify_ssl')
+                            ->defaultTrue()
+                        ->end()
+                        ->scalarNode('username')
+                            ->info('The username of the API user')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                            ->validate()
+                                ->ifTrue(function ($username) {
+                                    return !is_string($username);
+                                })
+                                ->thenInvalid('The Attribute Aggregation API username should be a string')
+                            ->end()
+                        ->end()
+                        ->scalarNode('password')
+                            ->info('The password of the API user')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                            ->validate()
+                                ->ifTrue(function ($password) {
+                                    return !is_string($password);
+                                })
+                                ->thenInvalid('The Attribute Aggregation API password should be a string')
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ src/OpenConext/AttributeAggregationApiClientBundle/Tests/DependencyInjection/HttpClientConfigurationTest.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Copyright 2017 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\AttributeAggregationApiClientBundle\Tests\DependencyInjection;
+
+use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
+use OpenConext\AttributeAggregationApiClientBundle\DependencyInjection\Configuration;
+use OpenConext\AttributeAggregationApiClientBundle\Tests\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+
+final class ConfigurationTest extends TestCase
+{
+    use ConfigurationTestCaseTrait;
+    use DataProvider;
+
+    private $validConfig = [
+        'base_url' => 'http://aa.invalid/',
+        'username' => 'test user',
+        'password' => 'test password',
+    ];
+
+    /**
+     * @test
+     * @group eb_api_config
+     * @dataProvider nonStringScalarProvider
+     */
+    public function base_url_cannot_be_other_than_string($value)
+    {
+        $config = $this->validConfig;
+        $config["base_url"] = $value;
+
+        $this->assertPartialConfigurationIsInvalid(
+            [['http_client' => $config]],
+            'http_client',
+            "The Attribute Aggregation API base URL should be a string"
+        );
+    }
+
+    /**
+     * @test
+     * @group eb_api_config
+     */
+    public function base_url_has_to_be_valid_url()
+    {
+        $config = $this->validConfig;
+        $config["base_url"] = "not a valid URL";
+
+        $this->assertPartialConfigurationIsInvalid(
+            [['http_client' => $config]],
+            'http_client',
+            "The Attribute Aggregation API base URL should be a valid URL"
+        );
+    }
+
+    /**
+     * @test
+     * @group eb_api_config
+     */
+    public function base_url_must_end_in_forward_slash()
+    {
+        $config = $this->validConfig;
+        $config["base_url"] = "https://aa.invalid";
+
+        $this->assertPartialConfigurationIsInvalid(
+            [['http_client' => $config]],
+            'http_client',
+            "The Attribute Aggregation API base URL must end in a forward slash"
+        );
+    }
+
+    /**
+     * @test
+     * @group eb_api_config
+     * @dataProvider nonStringScalarProvider
+     */
+    public function username_cannot_be_other_than_string($value)
+    {
+        $config = $this->validConfig;
+        $config["username"] = $value;
+
+        $this->assertPartialConfigurationIsInvalid(
+            [['http_client' => $config]],
+            'http_client',
+            "The Attribute Aggregation API username should be a string"
+        );
+    }
+
+    /**
+     * @test
+     * @group eb_api_config
+     * @dataProvider nonStringScalarProvider
+     */
+    public function password_cannot_be_other_than_string($value)
+    {
+        $config = $this->validConfig;
+        $config["password"] = $value;
+
+        $this->assertPartialConfigurationIsInvalid(
+            [['http_client' => $config]],
+            'http_client',
+            "The Attribute Aggregation API password should be a string"
+        );
+    }
+
+    protected function getConfiguration()
+    {
+        return new Configuration();
+    }
+}
