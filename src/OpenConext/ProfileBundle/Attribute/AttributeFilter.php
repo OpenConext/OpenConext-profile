6aed0b31c1ecb083a5cec41bb790059094ed46e2
Michiel Kodde <mkodde@ibuildings.nl>
2017-11-23 15:05:22 +0100

    Add information request endpoint
    The information request can be loaded at the /information-request
    route. It will display a filtered list of the users attributes and
    features a 'mail to servicedesk' button. This button will trigger a mail
    action lateron.
    
    An AttributeFilter was created, which is filtering all but these
    attributes:
    
     - commonName
     - displayName
     - mail
     - uid
     - eduPersonPrincipalName
     - affiliation
     - schacHomeOrganization

---
+++ src/OpenConext/ProfileBundle/Attribute/AttributeFilter.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright 2017 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\ProfileBundle\Attribute;
+
+use Surfnet\SamlBundle\SAML2\Attribute\AttributeSet;
+use Surfnet\SamlBundle\SAML2\Attribute\Attribute;
+
+final class AttributeFilter
+{
+    private static $filterValues = [
+        'commonName',
+        'displayName',
+        'mail',
+        'uid',
+        'eduPersonPrincipalName',
+        'affiliation',
+        'schacHomeOrganization',
+    ];
+
+    public function filter(AttributeSet $attributeSet)
+    {
+        /** @var Attribute[] $attributes */
+        $attributes = $attributeSet->getIterator()->getArrayCopy();
+        foreach ($attributes as $index => $attribute) {
+            $attributeName = $attribute->getAttributeDefinition()->getName();
+            if (!in_array($attributeName, self::$filterValues)) {
+                unset($attributes[$index]);
+            }
+        }
+        return AttributeSet::create($attributes);
+    }
+}
---
+++ src/OpenConext/ProfileBundle/Controller/InformationRequestController.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Copyright 2017 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace OpenConext\ProfileBundle\Controller;
+
+use OpenConext\ProfileBundle\Attribute\AttributeFilter;
+use OpenConext\ProfileBundle\Security\Guard;
+use OpenConext\ProfileBundle\Service\AttributeSupportMailService;
+use OpenConext\ProfileBundle\Service\UserService;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Templating\EngineInterface;
+
+class InformationRequestController
+{
+    /**
+     * @var Guard
+     */
+    private $guard;
+
+    /**
+     * @var EngineInterface
+     */
+    private $templateEngine;
+
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    /**
+     * @var AttributeSupportMailService
+     */
+    private $attributeSupportMailService;
+
+    /**
+     * @var AttributeFilter
+     */
+    private $attributeFilter;
+
+    public function __construct(
+        Guard $guard,
+        EngineInterface $templateEngine,
+        FormFactoryInterface $formFactory,
+        UrlGeneratorInterface $urlGenerator,
+        UserService $userService,
+        AttributeFilter $attributeFilter,
+        AttributeSupportMailService $attributeSupportMailService
+    ) {
+        $this->guard = $guard;
+        $this->templateEngine = $templateEngine;
+        $this->formFactory = $formFactory;
+        $this->urlGenerator = $urlGenerator;
+        $this->userService = $userService;
+        $this->attributeFilter = $attributeFilter;
+        $this->attributeSupportMailService = $attributeSupportMailService;
+    }
+
+    public function overviewAction()
+    {
+        $this->guard->userIsLoggedIn();
+
+        $informationRequestMailForm = $this->formFactory->create(
+            'profile_information_request_mail',
+            null,
+            ['action' => $this->urlGenerator->generate('profile.information_request_send_mail')]
+        );
+
+        $attributes = $this->attributeFilter->filter($this->userService->getUser()->getAttributes());
+
+        return new Response(
+            $this->templateEngine->render(
+                'OpenConextProfileBundle:InformationRequest:overview.html.twig',
+                [
+                    'attributes' => $attributes,
+                    'informationRequestMailForm' => $informationRequestMailForm->createView()
+                ]
+            )
+        );
+    }
+
+    public function sendMailAction()
+    {
+        $this->guard->userIsLoggedIn();
+
+        $this->attributeSupportMailService->sendAttributeSupportMail();
+
+        return new RedirectResponse($this->urlGenerator->generate('profile.information_request_confirm_mail_sent'));
+    }
+
+    public function confirmMailSentAction()
+    {
+        $this->guard->userIsLoggedIn();
+
+        return new Response(
+            $this->templateEngine->render('OpenConextProfileBundle:InformationRequest:confirmation.html.twig')
+        );
+    }
+}
