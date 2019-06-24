04add470cc5b7435ee793f694b8c2f3e923c5a4b
Michiel Kodde <mkodde@ibuildings.nl>
2019-06-24 14:17:45 +0200

    Drop IE9 support
    Maybe this will be reversed over time. For now these shims and fixes are
    removed from code

--- web/js/app.js
+++ web/js/app.js
@@ -1,14 +1,8 @@
-require('../../node_modules/material-design-lite/dist/material.min.js');
-require('./jquery-1.11.3.min.js');
-
-require('./es5-shim.min.js');
-require('./classList.min.js');
-require('./selectivizr.min.js');
-
 require('./main.js');
+require('../../node_modules/material-design-lite/dist/material.min.js');
+require('./jquery-3.4.1.min.js');
 
 require('../../node_modules/material-design-lite/dist/material.min.css');
 require('../css/roboto.css');
 require('../css/font-awesome.min.css');
 require('../css/main.css');
-require('../css/lte-ie9.css');
