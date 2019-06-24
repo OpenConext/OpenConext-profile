d51c155eaa69da89b81654842192d04f658409d5
Michiel Kodde <mkodde@ibuildings.nl>
2019-06-24 13:20:51 +0200

    Configure existing JS and CSS into Encore

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,8 @@ Encore
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
-     */
-    .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('app', './web/js/app.js')
+    .addEntry('my-connections', './web/js/my-connections.js')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
