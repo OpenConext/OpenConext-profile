c43b47c2a6962d83b86f40b3f9c0d1f0a960571d
Michiel Kodde <mkodde@ibuildings.nl>
2019-06-25 09:26:04 +0200

    Copy images to build folder and update templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-// directory where compiled assets will be stored
     .setOutputPath('web/build/')
-    // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+
+    .copyFiles({
+        from: './web/images/',
+        to: 'images/[path][name].[hash:8].[ext]',
+    })
 
     .addEntry('app', './web/js/app.js')
     .addEntry('my-connections', './web/js/my-connections.js')
@@ -20,14 +21,6 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
-
-// uncomment if you use Sass/SCSS files
-//.enableSassLoader()
-
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
