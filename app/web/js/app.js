23a82f61fb06a551fc27fe7ce83a31f508b21e11
Koen Cornelis <kcornelis@ibuildings.nl>
2021-06-29 13:39:46 +0200

    Style basic layout
    This commit styles the basic layout.  It:
    - ensures the various part all get their own file, both in templating and (s)css
    - removes a ton of now obsolete code (mostly mdl-oriented).
    - adds a ton of scss copied over from the eb styling.  Mostly these are helpers, but also some generic css.  Several adjustments have been made for this project.

--- app/web/js/app.js
+++ app/web/js/app.js
@@ -1,8 +1,3 @@
 require('jquery');
-require('material-design-lite/dist/material.min.js');
-
 require('./main.js');
-
-require('../css/roboto.scss');
-require('../css/font-awesome.min.scss');
-require('../css/main.scss');
+require('../css/application.scss');
