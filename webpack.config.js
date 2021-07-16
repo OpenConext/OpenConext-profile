ec5ba79cbc7d78bd90a231f9b9ad191777e65aaf
Koen Cornelis <kcornelis@ibuildings.nl>
2021-07-16 15:39:35 +0200

    Comply with the Symfony4 directory structure

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ Encore
     .setPublicPath('/build')
 
     .copyFiles({
-        from: './app/web/images/',
+        from: './assets/static/images/',
         to: 'images/[path][name].[hash:8].[ext]',
     })
 
-    .addEntry('app', './app/web/js/app.js')
-    .addEntry('my-connections', './app/web/js/my-connections.js')
+    .addEntry('app', './assets/js/app.js')
+    .addEntry('my-connections', './assets/js/my-connections.js')
 
     .disableSingleRuntimeChunk()
 
