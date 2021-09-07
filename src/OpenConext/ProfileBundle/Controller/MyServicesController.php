ba179a433fd1c880c963351bd9baf26220132e53
Michiel Kodde <mkodde@ibuildings.nl>
2021-09-07 13:25:36 +0200

    Redirect away from the delete route

--- src/OpenConext/ProfileBundle/Controller/MyServicesController.php
+++ src/OpenConext/ProfileBundle/Controller/MyServicesController.php
@@ -22,8 +22,10 @@ use OpenConext\Profile\Api\AuthenticatedUserProviderInterface;
 use OpenConext\ProfileBundle\Service\SpecifiedConsentListService;
 use OpenConext\ProfileBundle\Security\Guard;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Templating\EngineInterface;
 
 class MyServicesController
@@ -44,6 +46,11 @@ class MyServicesController
     private $authenticatedUserProvider;
 
     /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
      * @var Guard
      */
     private $guard;
@@ -53,25 +60,20 @@ class MyServicesController
      */
     private $logger;
 
-    /**
-     * @param EngineInterface $templateEngine
-     * @param AuthenticatedUserProviderInterface $authenticatedUserProvider
-     * @param SpecifiedConsentListService $specifiedConsentListService
-     * @param Guard $guard
-     * @param LoggerInterface $logger
-     */
     public function __construct(
         EngineInterface $templateEngine,
         AuthenticatedUserProviderInterface $authenticatedUserProvider,
         SpecifiedConsentListService $specifiedConsentListService,
         Guard $guard,
+        UrlGeneratorInterface $urlGenerator,
         LoggerInterface $logger
     ) {
-        $this->templateEngine              = $templateEngine;
-        $this->authenticatedUserProvider   = $authenticatedUserProvider;
+        $this->templateEngine = $templateEngine;
+        $this->authenticatedUserProvider = $authenticatedUserProvider;
         $this->specifiedConsentListService = $specifiedConsentListService;
-        $this->guard                       = $guard;
-        $this->logger                      = $logger;
+        $this->guard = $guard;
+        $this->urlGenerator = $urlGenerator;
+        $this->logger = $logger;
     }
 
     public function overviewAction(Request $request)
@@ -94,19 +96,11 @@ class MyServicesController
         ));
     }
 
-    public function deleteAction(string $serviceEntityId)
+    public function deleteAction(string $serviceEntityId): Response
     {
         $this->guard->userIsLoggedIn();
         $this->logger->notice('User wants to delete his info from a service with id: ' . $serviceEntityId);
-
-        $isDeleted = $this->specifiedConsentListService->deleteServiceWith($serviceEntityId);
-
-        $user = $this->authenticatedUserProvider->getCurrentUser();
-        $specifiedConsentList = $this->specifiedConsentListService->getListFor($user);
-
-        return new Response($this->templateEngine->render(
-            'OpenConextProfileBundle:MyServices:overview.html.twig',
-            ['specifiedConsentList' => $specifiedConsentList]
-        ));
+        $this->specifiedConsentListService->deleteServiceWith($serviceEntityId);
+        return new RedirectResponse($this->urlGenerator->generate('profile.my_services_overview'));
     }
 }
