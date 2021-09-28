600bd51b5ca8d0d8fb183789ecd308e298c89e69
Koen Cornelis <kcornelis@ibuildings.nl>
2021-09-28 09:43:04 +0200

    Fix malfunctioning a11y test
    Prior to this change the myservices a11y test failed because we use a link as a button to open/close the modal.  We do this because it allows us to open/close the modal without js, meaning it still works even if JS isn't loaded on the page.
    
    HTML Validator, however, has a rule on by default to prefer native elements over using aria roles.  This is a good rule and up till recently we could only disable it globally.  After filing an issue with cypress-html-validator it is now possible to override the configuration of html-validator on a per-call basis.
    
    This change applies the disabling of the prefer-native-elements rule for the button element on the myservices page.

--- tests/cypress/integration/a11y/myServices.a11y.spec.js
+++ tests/cypress/integration/a11y/myServices.a11y.spec.js
@@ -19,7 +19,24 @@ context('verify a11y of my services page', () => {
                 cy.visit('https://profile.vm.openconext.org/my-services').then(() => {
                     cy.wait(300).then(() => {
                         cy.removeSFToolbar();
-                        cy.htmlvalidate();
+                        cy.htmlvalidate({
+                            "rules": {
+                                "require-sri": [ "error", {
+                                    "target": "crossorigin",
+                                }],
+                                /**
+                                 * Heading-level:
+                                 * This allows the page to have a h2 before a h1 (necessary due to navigation).
+                                 */
+                                "heading-level": [ "error", {
+                                    "minInitialRank": "h2"
+                                }],
+                                "prefer-native-element": [
+                                  "error", {
+                                        "exclude": [ "button" ],
+                                }],
+                            },
+                        });
                     });
                 });
             });
