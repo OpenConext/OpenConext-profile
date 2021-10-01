b340e7c4422426694327f0ec6e9188a263ecea3a
Koen Cornelis <kcornelis@ibuildings.nl>
2021-10-01 14:20:19 +0200

    Readd app.js

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+require('./polyfills.js');
+require('./nojs.js');
+require('./dialog.js');
+require('./localeSwitch.js');
+require('./mobileMenu.js');
+require('./modalWindow.js');
+require('./tooltip.js');
+require('../css/application.scss');
