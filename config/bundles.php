8e0991c6224fd11f4e8a7aa1ba00e18ea0a8bc05
Vadim Schmitz <di3mpi3@gmail.com>
2022-04-25 13:03:52 +0200

    Remove Swiftmailer

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
